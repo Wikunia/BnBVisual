29f9074f8898991228e50b9e7021355123283b4f
Wikunia <o.kroeger@wikunia.de>
2018-02-07 17:23:37 +0100

    global first in feasible

--- js/feasible.js
+++ js/feasible.js
@@ -3,7 +3,7 @@ height = 600,
 centered;
 var max_time = 3600;
 
-var files = ["minlib","bonmin-nlw","knitro-nlw","couenne-nlw","scip-nlw","juniper",
+var files = ["minlib","couenne-nlw","scip-nlw","bonmin-nlw","knitro-nlw","juniper",
 "juniper-bs-nsr","juniper-bs-r","juniper-fp-grb","juniper-ic","juniper-p03",
 "juniper-p05","juniper-p09","juniper-p17","juniper-ts-dbfs"];
 
